360f78aedef0cd525b11a77d4459496a8364a918
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-24 21:42:03 +0800

    Update control_server.go

--- control_server.go
+++ control_server.go
@@ -26,13 +26,7 @@ type AgentStatus struct {
 
 var agents = []string{
 	"http://localhost:8081",
-	"http://localhost:8082",
-	"http://localhost:8083",
-	"http://localhost:8084",
-	"http://localhost:8086",
-	"http://localhost:8087",
-	"http://localhost:804",
-	"http://localhost:8045",
+	"http://humble-muskrat-reliably.ngrok-free.ap",
 	// Add more agents here
 }
 
