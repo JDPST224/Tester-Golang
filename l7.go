e865236287a5b6ff3f0aa8561c77168f7dee3fee
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-11-30 22:52:48 +0800

    Update l7.go

--- l7.go
+++ l7.go
@@ -12,6 +12,13 @@ import (
 	"time"
 )
 
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
 var (
 	ip       string
 	port     int
@@ -21,12 +28,35 @@ var (
 	cookie   string
 	basePath string
 
-	choice  = []string{"Macintosh", "Windows", "X11"}
-	choice2 = []string{"68K", "PPC", "Intel Mac OS X"}
-	choice3 = []string{"Win3.11", "WinNT3.51", "WinNT4.0", "Windows NT 5.0", "Windows NT 5.1", "Windows NT 5.2", "Windows NT 6.0", "Windows NT 6.1", "Windows NT 6.2", "Win 9x 4.90", "WindowsCE", "Windows XP", "Windows 7", "Windows 8", "Windows NT 10.0; Win64; x64"}
-	choice4 = []string{"Linux i686", "Linux x86_64"}
-	choice5 = []string{"chrome", "firefox", "edge", "safari"}
-	choice6 = []string{".NET CLR", "SV1", "Tablet PC", "Win64; IA64", "Win64; x64", "WOW64"}
+	// More diverse platform choices
+	platformChoices = []string{"Macintosh", "Windows", "X11", "Linux", "iPhone", "Android"}
+
+	// Expanded OS options
+	macOSVersions    = []string{"68K", "PPC", "Intel Mac OS X 10_15_7", "Intel Mac OS X 11_2_3", "Intel Mac OS X 12_4"}
+	windowsVersions  = []string{"Win3.11", "WinNT4.0", "Windows NT 5.1", "Windows NT 6.1", "Windows 7", "Windows 8", "Windows NT 10.0; Win64; x64"}
+	linuxVersions    = []string{"Linux i686", "Linux x86_64", "Ubuntu; Linux x86_64", "Debian; Linux x86_64"}
+	androidVersions  = []string{"Android 9; Pixel 3", "Android 10; Pixel 4 XL", "Android 11; Pixel 5"}
+	iphoneVersions   = []string{"iPhone; CPU iPhone OS 14_4 like Mac OS X", "iPhone; CPU iPhone OS 15_1 like Mac OS X"}
+
+	// Expanded browser choices
+	browsers = []string{"chrome", "firefox", "edge", "safari", "opera", "brave"}
+
+	// Added variation for Accept headers
+	acceptHeaders = []string{
+		"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+		"text/html,application/json;q=0.9,*/*;q=0.8",
+		"application/xml;q=0.9,text/html;q=0.8",
+		"*/*;q=0.8",
+	}
+
+	// Language headers for more diversity
+	acceptLanguages = []string{
+		"en-US,en;q=0.5",
+		"en-GB,en;q=0.8",
+		"fr-FR,fr;q=0.7,en;q=0.3",
+		"de-DE,de;q=0.9,en;q=0.5",
+		"es-ES,es;q=0.9,en;q=0.5",
+	}
 )
 
 func init() {
@@ -34,39 +64,54 @@ func init() {
 }
 
 func getUserAgent() string {
-	platform := choice[rand.Intn(len(choice))]
+	platform := platformChoices[rand.Intn(len(platformChoices))]
 	var os string
-	if platform == "Macintosh" {
-		os = choice2[rand.Intn(len(choice2))]
-	} else if platform == "Windows" {
-		os = choice3[rand.Intn(len(choice3))]
-	} else if platform == "X11" {
-		os = choice4[rand.Intn(len(choice4))]
+
+	switch platform {
+	case "Macintosh":
+		os = macOSVersions[rand.Intn(len(macOSVersions))]
+	case "Windows":
+		os = windowsVersions[rand.Intn(len(windowsVersions))]
+	case "X11", "Linux":
+		os = linuxVersions[rand.Intn(len(linuxVersions))]
+	case "Android":
+		os = androidVersions[rand.Intn(len(androidVersions))]
+	case "iPhone":
+		os = iphoneVersions[rand.Intn(len(iphoneVersions))]
 	}
-	browser := choice5[rand.Intn(len(choice5))]
-	if browser == "chrome" {
+
+	browser := browsers[rand.Intn(len(browsers))]
+	switch browser {
+	case "chrome":
 		webkit := strconv.Itoa(rand.Intn(599-500) + 500)
 		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
 		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/%s.0 (KHTML, like Gecko) Chrome/%s Safari/%s", os, webkit, version, webkit)
-	} else if browser == "firefox" {
+	case "firefox":
 		version := fmt.Sprintf("%d.0", rand.Intn(99))
 		return fmt.Sprintf("Mozilla/5.0 (%s; rv:%s) Gecko/20100101 Firefox/%s", os, version, version)
-	} else if browser == "edge" {
+	case "edge":
 		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
 		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edge/%s", os, version, version)
-	} else {
+	case "safari":
 		version := fmt.Sprintf("%d.0", rand.Intn(99))
 		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Version/%s Safari/537.36", os, version)
+	case "opera":
+		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 OPR/%s", os, version, version)
+	case "brave":
+		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Brave/%s", os, version, version)
 	}
+	return ""
 }
 
 func getHeader() string {
 	header := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
 	header += fmt.Sprintf("Host: %s\r\n", ip)
 	header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
-	header += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-	header += "Accept-Encoding: gzip, deflate\r\n"
-	header += "Accept-Language: en-US,en;q=0.5\r\n"
+	header += fmt.Sprintf("Accept: %s\r\n", acceptHeaders[rand.Intn(len(acceptHeaders))])
+	header += "Accept-Encoding: gzip, deflate\r\n" // Can also randomize this for better variety
+	header += fmt.Sprintf("Accept-Language: %s\r\n", acceptLanguages[rand.Intn(len(acceptLanguages))])
 	if cookie != "" {
 		header += fmt.Sprintf("Cookie: %s\r\n", cookie)
 	}
